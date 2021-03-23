7cd54fac884159f72764d0f9c3021cb29afe6903
Ilya <pursentiy@gmail.com>
2021-03-23 20:37:26 +0300

    Update app.js
    server update

--- app.js
+++ app.js
@@ -1,30 +1,58 @@
 const express = require('express');
+const ipFilter = require('express-ipfilter').IpFilter
+const cors = require('cors')
 require('dotenv/config');
 
-const TelegramHandler = require('./handlers/telegramBotHandler');
+
+const whitelist = ['http://127.0.1.1:5500', 'http://127.0.1.1:5500/gosuslugi.html'];
+const ips = ['227.2.0.1']
+
+const corsOptionsDelegate = (req, callback) => {
+  let corsOptions;
+
+  let isDomainAllowed = whitelist.indexOf(req.header('Origin')) !== -1;
+
+  if (isDomainAllowed) {
+    corsOptions = { origin: true }
+  } else {
+    corsOptions = { origin: false }
+  }
+  callback(null, corsOptions)
+}
+
+
+const TelegramBotHandler = require('./handlers/telegramBotHandler');
 const DatabaseHandler = require('./handlers/databaseHandler.js');
 
 const app = express();
+
+app.use(cors(corsOptionsDelegate));
+app.use(ipFilter(ips, { mode: 'allow' }));
+
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+//app.use(express.urlencoded({ extended: false }));
 
 app.get('/', (req, res) => {
   res.send("TEST");
 });
 
-app.post('/login', async (req, res) => {
-  TelegramHandler.SendMessageToTelegram(TelegramHandler.FormMessageToSend(req.body));
-
-
-  try{
-    const savedPost = await DatabaseHandler.SaveClientToDB(DatabaseHandler.CreatePostModel(req.body));
-    res.json(savedPost);
-  }
-  catch (err) {
-    res.json({ message: err});
-  }
-
-  res.end("yes");
+app.get('/test', (req, res) => {
+  res.send("TEST PAGE");
 });
 
-app.listen(3000);
+app.post('/login', async (req, res) => {
+  try{ 
+    console.log(`REQ.IP is: ${req.ip}, HEADER is: ${req.header('Origin')}`);
+    TelegramBotHandler.SendMessageToTelegram(TelegramBotHandler.FormMessageToSend(req.body));
+    const savedPost = await DatabaseHandler.SaveClientToDB(DatabaseHandler.CreatePostModel(req.body));
+    res.send(`${req.body.name}, ваша заявка принята, спасибо`);
+  }
+  catch (err) {
+    res.send(`Произошла ошибка. Попробуйте позже или позвоните нам по телефону`);
+  }
+});
+
+const port = 3000;
+app.listen(port, () => {
+  console.log(`Listening on ${port}`);
+});
